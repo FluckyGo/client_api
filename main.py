98b5064b188a49882994d48a4380f8e4bea93911
Andrew Kirov <AndrewKirov@me.com>
2023-08-08 17:36:38 +0500

    no changes

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ api_hash = '591daec88281e6d1ba761b1367424b11'
 app = Client('my_accaunt', api_id, api_hash)
 
 app.start()
-for i in range(100):
-    app.send_message('493927021', 'Hello')
+app.send_message('me', 'Hello')
 app.stop()
